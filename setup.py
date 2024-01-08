1b6ef8dc8cc225b72093ce603bdd532f7e44973f
Matthew Hammond <FrozenDude101@outlook.com>
2024-01-08 16:12:01 +0000

    Add module setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "sitebuilder",
+    version = "0.1.0",
+    url = "https://github.com/FrozenDude101/sitebuilder.git",
+    author = "Matthew Hammond",
+    description = "A basic sitebuilder for my site, mammond.com.",
+    packages = ["sitebuilder"],
+    package_dir = {'':'src'}
+)
